f0c370af57f23770c25bc25426bf574f0a13d9c6
Andrey <a>
2018-10-17 20:59:57 +0300

    singelton select

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Connection...");
-        MyConnection.startConnection();
+        MyConnection myConnection = MyConnection.INSTANCE;
+        myConnection.printAllFIO();
+        myConnection.printAllFIOAndPhone();
     }
 }
--- src/main/java/MyConnection.java
+++ src/main/java/MyConnection.java
@@ -1,25 +1,67 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class MyConnection {
-    public static void startConnection() {
+    public static final MyConnection INSTANCE = new MyConnection();
+    private Connection connection = null;
+
+    private MyConnection() {
+    }
+
+    private void startConnection() {
         String url = "jdbc:postgresql://localhost/test1";
         Properties properties = new Properties();
         properties.setProperty("user", "postgres");
         properties.setProperty("password", "Qwer123");
-
-        Connection connection = null;
 
         try {
             connection = DriverManager.getConnection(url, properties);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
 
-        if (connection != null) {
-            System.out.println("connection successfully!");
+    public void printAllFIO() {
+        if (connection == null) {
+            startConnection();
         }
+
+        System.out.println("All people:");
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM person");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(3));
+            }
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void printAllFIOAndPhone() {
+        if (connection == null) {
+            startConnection();
+        }
+
+        System.out.println("All people and phone:");
+
+
+        try {
+            Statement statement = connection.createStatement();
+            String query = "SELECT * FROM person " +
+                    "LEFT OUTER JOIN address ON person.address_id = address.address_id";
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(3));
+            }
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
